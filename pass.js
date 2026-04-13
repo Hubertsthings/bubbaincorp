3cb2feaa3a6d23a0c135303b2f0f0f56246fbcdd
Coolhubert <1backup5acc@gmail.com>
2026-04-12 20:21:51 -0500

    new pass system

---
+++ pass.js
@@ -0,0 +1,20 @@
+
+  const users = {
+    "admin": "niceshoes",
+    "bob": "cool",
+    "john": "abc123"
+  };
+
+  function login() {
+    const u = document.getElementById("username").value;
+    const p = document.getElementById("password").value;
+
+    if (users[u] && users[u] === p) {
+      localStorage.setItem("loggedIn", "true");
+      localStorage.setItem("user", u);
+
+      window.location.href = "Premium.html";
+    } else {
+      document.getElementById("msg").textContent = "No";
+    }
+  }
